7b39bc44d33670e7e3c86e7980891467e053d856
dumars <114284649+nextbirthday@users.noreply.github.com>
2024-07-25 10:46:28 +0900

    Delete pretz directory

--- pretz/src/app/(route)/admin/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>adminLoginPage</div>
-  )
-}
-
-export default page;
--- pretz/src/app/(route)/member/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>member dashboard page</div>
-  )
-}
-
-export default page;
